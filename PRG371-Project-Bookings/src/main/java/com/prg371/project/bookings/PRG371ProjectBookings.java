a1c0d891a2133f72744cebb8b752311864a6e016
Ryan De Wet <ryan.dewet.rdw@outlook.com>
2024-10-15 18:15:57 +0200

    added java mavan project base

---
+++ PRG371-Project-Bookings/src/main/java/com/prg371/project/bookings/PRG371ProjectBookings.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.prg371.project.bookings;
+
+/**
+ *
+ * @author User
+ */
+public class PRG371ProjectBookings {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
